6451059e0fa30df8397b9534b021cdaccde8549d
nikolai2787 <nikolayytaran@gmail.com>
2022-05-19 21:49:12 +0300

    Version: 1.0

--- app/src/main/java/com/example/yourcharacter/MainActivity.java
+++ app/src/main/java/com/example/yourcharacter/MainActivity.java
@@ -1,14 +1,42 @@
 package com.example.yourcharacter;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.Guideline;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Spinner spinnerColor;
+    private TextView descriptionTemp;
+    private Guideline guideline;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Assigning elements
+        spinnerColor = findViewById(R.id.spinnerColor);
+        descriptionTemp = findViewById(R.id.textViewDescriptionTemp);
+        guideline = findViewById(R.id.guideline);
     }
+
+    public void showDescription(View view) {
+        //Get the position of an element
+        int position = spinnerColor.getSelectedItemPosition();
+        String description = getDescriptionByPosition(position);
+        if (description!=null && !description.isEmpty()) {
+            guideline.setGuidelinePercent(0);
+            descriptionTemp.setText(description);
+        }
+    }
+    //Element declaration method
+    private String getDescriptionByPosition(int position){
+        String[] descriptions = getResources().getStringArray(R.array.color_description);
+        return descriptions[position];
+    }
+
 }
